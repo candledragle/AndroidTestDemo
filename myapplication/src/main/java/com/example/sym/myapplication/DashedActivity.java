12612adc66cee3fae9a3a72dec9713eb3e2a1c1e
sym <s_abcabc@126.com>
2015-12-04 16:01:19 +0800

    add myapplication

---
+++ myapplication/src/main/java/com/example/sym/myapplication/DashedActivity.java
@@ -0,0 +1,16 @@
+package com.example.sym.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sym on 12/3/15.
+ */
+public class DashedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashed);
+    }
+}
